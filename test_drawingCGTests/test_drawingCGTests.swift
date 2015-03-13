6a85b7e306d120f0fe54a742712cb4c5a01edeb5
danaavesar <danaavesar@gmail.com>
2015-03-13 12:30:50 -0400

    Initial Commit

---
+++ test_drawingCGTests/test_drawingCGTests.swift
@@ -0,0 +1,36 @@
+//
+//  test_drawingCGTests.swift
+//  test_drawingCGTests
+//
+//  Created by Dana Avesar on 3/13/15.
+//  Copyright (c) 2015 Dana Avesar. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class test_drawingCGTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
